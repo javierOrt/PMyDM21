48dcca1d3fbb786fa6d7ba9bf42209df4fa15cbe
javit <javito19991@gmail.com>
2021-09-17 16:48:43 +0200

    17/09/2021

--- pmdm/src/P1.java
+++ pmdm/src/P1.java
@@ -1,3 +1,12 @@
 public class P1 {
-klk matatn que fue contigo subiendo vaina a github
+    public static void main(String[] args){
+        //imprimir la lista de palabras de tamaño impar de un texto
+
+        String sNombre = "alberto javi perro laveeeeeeerga nosej lambebicho asi se habla bbsitas";
+        String[] partes = sNombre.split(" ");
+
+        for (int i=0;i< partes.length;i++){
+            if(partes[i].length()%2 != 0) System.out.println(partes[i]);
+        }
+    }
 }
